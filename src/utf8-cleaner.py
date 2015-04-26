cd4ab2a0d4bae7d494a9c8fa22c5c517ad294fb1
zezhouliu <zezhouliu@college.harvard.edu>
2015-04-26 12:33:24 -0700

    Updated utf8-cleaner to iterate through data files

--- src/utf8-cleaner.py
+++ src/utf8-cleaner.py
@@ -1,13 +1,19 @@
 import codecs
 
-sourceFileName = "matches1.json"
-targetFileName = "matches_1.json"
+if __name__ == '__main__':
 
-BLOCKSIZE = 1048576 # or some other, desired size in bytes
-with codecs.open(sourceFileName, "r", "iso-8859-1") as sourceFile:
-    with codecs.open(targetFileName, "w", "utf-8") as targetFile:
-        while True:
-            contents = sourceFile.read(BLOCKSIZE)
-            if not contents:
-                break
-            targetFile.write(contents)
+
+	BLOCKSIZE = 1048576 # or some other, desired size in bytes
+
+	sourceFiles = ['matches1.json']
+	targetFiles = ['m1.json']
+	for i in xrange(len(sourceFiles)):
+		sourceFileName = sourceFiles[i]
+		targetFileName = targetFiles[i]
+		with codecs.open(sourceFileName, "r", "iso-8859-1") as sourceFile:
+		    with codecs.open(targetFileName, "w", "utf-8") as targetFile:
+		        while True:
+		            contents = sourceFile.read(BLOCKSIZE)
+		            if not contents:
+		                break
+		            targetFile.write(contents)
